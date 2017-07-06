d8abf31309f0d318f399f9119af3ac5c444a274d
mishkadoma <mixandoma@ya.ru>
2017-07-06 15:18:23 +0500

    argv usage

---
+++ argv_demonstration.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+first, second, third, forth = argv
+
+print "The name of file: %s" % first
+print "First argument: ", second
+print "Second argument: ", third
+print "the last one is: ", forth
